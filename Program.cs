ec548cbea426c06dae6ffbd35d81777ee3ab5753
Mayara Abreu <93221609+mayaraarj@users.noreply.github.com>
2022-09-28 14:12:28 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jogo_De_Xadrez_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
