6382c9b5f542be29d9c5fbb8a06915e9d8d0bb48
Mayara Abreu <93221609+mayaraarj@users.noreply.github.com>
2022-09-28 14:50:31 -0300

    Criando a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Jogo_De_Xadrez_Console
+﻿using System;
+using Tabuleiro;
+
+namespace Jogo_De_Xadrez_Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Posicao P;
+
+            P = new Posicao(3,4);
+
+            Console.WriteLine("Posição: "+ P);
+
         }
     }
 }
