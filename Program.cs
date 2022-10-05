3a0e0c3204cadab3f40a5faf27c36a2c41320229
Mayara Abreu <93221609+mayaraarj@users.noreply.github.com>
2022-10-05 13:42:05 -0300

    Comecando a implementar o tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace Jogo_De_Xadrez_Console
 {
@@ -7,12 +7,9 @@ namespace Jogo_De_Xadrez_Console
     {
         static void Main(string[] args)
         {
-
-            Posicao P;
-
-            P = new Posicao(3,4);
-
-            Console.WriteLine("Posição: "+ P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine();
+          
 
         }
     }
--- Tabuleiro/Posicao.cs
+++ Tabuleiro/Posicao.cs
@@ -1,19 +1,19 @@
-﻿namespace Tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
-        public int linha { get; set; }
-        public int coluna { get; set; }
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
 
         public Posicao(int linha, int coluna)
         {
-            this.linha = linha;
-            this.coluna = coluna;
+            this.Linha = linha;
+            this.Coluna = coluna;
         }
 
         public override string ToString()
         {
-            return linha +","+ coluna;
+            return Linha +","+ Coluna;
         }
     }
 }
